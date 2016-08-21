598bbff47a8efd5977b2f0977788fa04c64b3553
kengardiner <ken_gardiner@yahoo.com>
2016-08-21 19:58:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/heroapp/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.heroapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.heroapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
