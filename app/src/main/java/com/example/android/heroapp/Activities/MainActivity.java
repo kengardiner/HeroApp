7bf566fb3c948c825ce26fab9cdc66f1ac56698b
Ken Gardiner <ken_gardiner@yahoo.com>
2016-08-23 09:31:45 -0400

    Fragment Connected

--- app/src/main/java/com/example/android/heroapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/android/heroapp/Activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android.heroapp.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 
+import com.example.android.heroapp.Fragments.MainFragment;
 import com.example.android.heroapp.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+        }
     }
 }
